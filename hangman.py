516c4ce7a08a1706c72bac3fba1e2d28ad6eb9fb
riley fitz <rileyfitzgibbons@gmail.com>
2022-01-14 22:58:21 +0100

    Added hangman graphic.

--- hangman.py
+++ hangman.py
@@ -5,24 +5,51 @@ from sys import argv
 def main():
     # Load word bank
     words = get_wordbank()
-    for word in words:
-        print(word)
+    word = choice(words).strip()
+    print(word)
 
 def get_wordbank(wordbank='wordbank.csv'):
     '''
-    Loads a csv file, and returns a list of words contained. 
+    Loads a csv file, and returns a list of words contained.
+    Defaults to wordbank.csv but supports external csv files.
     '''
     try:
         if len(argv) > 1:
             wordbank = argv[1]
         with open(wordbank, newline='') as csvfile:
             csv_words =  reader(csvfile)
-            words = list(csv_words)
-            return words[0]
+            words = list(csv_words)[0]
+            return words
     except Exception as e:
         print(e)
         print("There above error occured while trying to load the wordbank..\nQuitting")
         quit()
+
+def print_hangman(guesses=0):
+    '''
+    I know the if's ain't pretty, it's what I got atm.
+    '''
+    head, torso, larm, rarm, lleg, rleg = '','',' ','','',''
+
+    if guesses >= 1:
+        head = 'O'
+    if guesses >= 2:
+        torso = '|'
+    if guesses >= 3:
+        larm = '/'
+    if guesses >= 4:
+        rarm = '\\'
+    if guesses >= 5:
+        lleg = '/'
+    if guesses >= 6:
+        rleg = '\\'
+
+    print('_______')
+    print(f'|    |')
+    print(f'|    {head}')
+    print(f'|   {larm}{torso}{rarm} ')
+    print(f'|   {lleg} {rleg} ')
+    print('|_______')
 
 if __name__=="__main__":
     main()

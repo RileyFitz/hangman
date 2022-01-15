70ab9b5d424c9be5bba7d7ef84b661155a034cd3
riley fitz <rileyfitzgibbons@gmail.com>
2022-01-15 15:22:41 +0100

    Input validation and seperated game for clarity.

--- hangman.py
+++ hangman.py
@@ -7,6 +7,34 @@ from string import ascii_lowercase
 def main():
     # Load word bank
     word = select_from_wordbank()
+    win = play_game(word)
+
+def play_game(word):
+    # Initialize vars
+    used = ''
+    unused = ascii_lowercase
+    attempts = 0
+    win = False
+
+    while attempts < 6 and not win:
+        print_hangman(attempts)
+        # print_blanks
+        curr_guess = validated_guess()
+
+def validated_guess():
+    '''
+    Ensures that only a single letter can be returned as an attempted guess.
+    '''
+    while True:
+        try:
+            inp = input().lower()
+            if len(inp) > 1 or inp.isdigit():
+                print("Guess's must be a single letter. Please try again..")
+            else:
+                return inp
+        except Exception as e:
+            print(f'The following exception occured.\n{e}')
+            print('Please try an input again.')
 
 def select_from_wordbank(wordbank='wordbank.csv'):
     '''

779747412ef97eccb0c1791f51b26a2518e60f29
riley fitz <rileyfitzgibbons@gmail.com>
2022-05-06 09:07:59 -0600

    Add embedded_ttt game logic.

--- TicTacToe/embedded_ttt.py
+++ TicTacToe/embedded_ttt.py
@@ -2,7 +2,7 @@ from ttt import ttt_board
 
 class embedded_ttt_board(ttt_board):
     def __init__(self):
-        self.winner = ''
+        super().__init__()
         self.board_state = []
         for i in range(9):
             self.board_state.append(ttt_board())
@@ -22,8 +22,23 @@ class embedded_ttt_board(ttt_board):
         '''
         Returns an int representing a valid board without a winner.
         '''
+        print('Please select a board to make a move on..')
         board = self.get_validated_move()
         if self.board_state[board].winner != "":
             print(f'This board has been won by {self.board_state[board].winner}\nTry a different board')
             board =  self.get_validated_board()
         return int(board)
+
+    def initiate_game(self):
+        while self.winner == '':
+            # print_board
+            self.print_board()
+            # get emb_board input
+            board = self.get_validated_board()
+            # Make move on validated board.
+            move = self.board_state[board].get_validated_move()
+            self.board_state[board].board_state[move] = 'X' if self.xTurn else 'O'
+            #Check winner on said board
+            self.board_state[board].check_winner()
+            # Change player
+            self.xTurn = not self.xTurn

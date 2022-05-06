77630885d55b73c0b5fa721980960522c0ac7b3a
riley fitz <rileyfitzgibbons@gmail.com>
2022-05-06 08:47:19 -0600

    Add logic to return validated board int.

--- TicTacToe/embedded_ttt.py
+++ TicTacToe/embedded_ttt.py
@@ -2,6 +2,7 @@ from ttt import ttt_board
 
 class embedded_ttt_board(ttt_board):
     def __init__(self):
+        self.winner = ''
         self.board_state = []
         for i in range(9):
             self.board_state.append(ttt_board())
@@ -13,15 +14,16 @@ class embedded_ttt_board(ttt_board):
                 print(f'\
  {self.board_state[i*3].board_state[j*3]}|{self.board_state[i*3].board_state[j*3+1]}|{self.board_state[i*3].board_state[j*3+2]} | \
  {self.board_state[i*3+1].board_state[j*3]}|{self.board_state[i*3+1].board_state[j*3+1]}|{self.board_state[i*3+1].board_state[j*3+2]} | \
- {self.board_state[i*3+1].board_state[j*3]}|{self.board_state[i*3+2].board_state[j*3+1]}|{self.board_state[i*3+2].board_state[j*3+2]}\
-')
+ {self.board_state[i*3+1].board_state[j*3]}|{self.board_state[i*3+2].board_state[j*3+1]}|{self.board_state[i*3+2].board_state[j*3+2]}')
             if i != 2:
                 print('~~~~~~~~~~~~~~~~~~~~~~~~~')
 
-    def get_validated_move_emb(self):
-        attempt = self.get_validated_move()
-        if self.board_state[attempt].winner != "":
-            print(f'This board has been won by {self.board_state[attempt].winner}\nTry a different board')
-            print(f'{attempt}')
-            attempt = self.get_validated_move_emb()
-        return attempt
+    def get_validated_board(self):
+        '''
+        Returns an int representing a valid board without a winner.
+        '''
+        board = self.get_validated_move()
+        if self.board_state[board].winner != "":
+            print(f'This board has been won by {self.board_state[board].winner}\nTry a different board')
+            board =  self.get_validated_board()
+        return int(board)

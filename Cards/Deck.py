a2d79ff0c5fa946845a66184b4938a037234fd66
Riley Fitzgibbons <rileyfitzgibbons@gmail.com>
2023-04-16 19:53:21 -0600

    Add colors to card objects

--- Cards/Deck.py
+++ Cards/Deck.py
@@ -1,15 +1,16 @@
 from random import shuffle
 
 class Card():
-    def __init__(self, suit, value):
+    def __init__(self, suit, value, color):
         self.suit = suit
         self.value = value
+        self.color = color
 
     def print_card(self):
         print(f'{self.value} of {self.suit}')
 
     def return_vals(self):
-        return value, suit
+        return value, suit, color
         
 class Deck():
     def __init__(self):
@@ -19,9 +20,9 @@ class Deck():
         suits = ['spades', 'clubs', 'diamonds', 'hearts']
         values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
         deck = []
-        for suit in suits:
+        for idx, suit in enumerate(suits):
             for value in values:
-                deck.append(Card(suit, value))
+                deck.append(Card(suit, value, 'black' if idx%2 == 0 else 'red'))
         return deck
         
     def shuffle_deck(self):

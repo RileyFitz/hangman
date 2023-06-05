8863cf0d3eed80bc3513dcc901d05c58a5f3062e
Riley Fitzgibbons <rileyfitzgibbons@gmail.com>
2023-06-04 22:06:29 -0600

    Create main, fix def

--- Cards/Solitaire.py
+++ Cards/Solitaire.py
@@ -5,9 +5,16 @@ class Solitaire():
         self.deck = Deck()
         self.board = [[],[],[],[],[],[],[]]
         
-    def serve_fresh_deal():
+    def serve_fresh_deal(self):
         self.deck.reset_deck()
         for rows in range(7):
             for columns in range(rows,7):
                 self.board[columns].append(self.deck.pop())
+def main():
+    """Set up game steps"""
+    x = Solitaire()
+    x.serve_fresh_deal()
+    print("main")
 
+if __name__=="__main__":
+    main()

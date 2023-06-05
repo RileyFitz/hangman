784e544b9f5376d925bd048dae5c0c65e77814ac
Riley Fitzgibbons <rileyfitzgibbons@gmail.com>
2023-06-04 21:56:02 -0600

    Fix def spacing

--- Cards/Solitaire.py
+++ Cards/Solitaire.py
@@ -5,9 +5,9 @@ class Solitaire():
         self.deck = Deck()
         self.board = [[],[],[],[],[],[],[]]
         
-        def serve_fresh_deal():
-            self.deck.reset_deck()
-            for rows in range(7):
-                for columns in range(rows,7):
-                    self.board[columns].append(self.deck.pop())
+    def serve_fresh_deal():
+        self.deck.reset_deck()
+        for rows in range(7):
+            for columns in range(rows,7):
+                self.board[columns].append(self.deck.pop())
 

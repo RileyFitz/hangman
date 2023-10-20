7d122d7e046e2c6c3a6c82b5692d0213aae7bde3
Riley Fitz <rileyfitzgibbons@gmail.com>
2023-10-19 21:23:38 -0600

    Revert "This is a hard break in the Solitaire game, to use GPT"
    This reverts commit 0f62c43c5e0d4ef6e1dd8c2946b9c2d5844c73d5.

--- Cards/Solitaire.py
+++ Cards/Solitaire.py
@@ -1,157 +1,65 @@
-import random
-import os
-from subprocess import call
+from Deck import Deck
+from string import ascii_uppercase
 
-def clear_screen():
-    # Windows
-    if os.name == "nt":
-        _ = call("cls", shell=True)
-    # Mac and Unix/Linux
-    else:
-        _ = call("clear")
-
-# Card class represents a single playing card
-class Card:
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.suit = suit
-        self.face_up = False
-
-    def __repr__(self):
-        return f"{self.rank}{self.suit}"
-
-
-# Deck class represents a deck of playing cards
-class Deck:
-    def __init__(self):
-        ranks = ["A"] + [str(i) for i in range(2, 11)] + ["J", "Q", "K"]
-        suits = ["♠", "♣", "♥", "♦"]
-        self.cards = [Card(rank, suit) for rank in ranks for suit in suits]
-        random.shuffle(self.cards)
-
-    def deal(self):
-        return self.cards.pop()
-
-    def is_empty(self):
-        return len(self.cards) == 0
-
-
-# SolitaireGame class represents the game and its logic
-class SolitaireGame:
+class Solitaire():
     def __init__(self):
         self.deck = Deck()
-        self.stock = []
-        self.waste = []
-        self.foundations = [[] for _ in range(4)]
-        self.tableau = [[] for _ in range(7)]
+        self.board = [[],[],[],[],[],[],[]]
+        self.aces = [[],[],[],[]]
+        self.win = False
+        
+    def fresh_deal(self):
+        self.deck.reset_deck()
+        self.deck.shuffle_deck()
+        for rows in range(7):
+            for columns in range(rows,7):
+                self.board[columns].append(self.deck.pop())
 
-    def deal_initial_cards(self):
-        for i in range(7):
-            for j in range(i + 1):
-                card = self.deck.deal()
-                if j == i:
-                    card.face_up = True
-                self.tableau[i].append(card)
+    def print_board(self):
+        print("   1    2    3    4    5    6    7")
+        print_board_object = self.gather_print_board_object()
+        for j in range(self.find_max_list()):
+            row = ascii_uppercase[j] + "  "
+            for i in range(7):
+                row += print_board_object[i][j] + "  "
+            print(row)
+            row = ""
 
-        while not self.deck.is_empty():
-            self.stock.append(self.deck.deal())
-
-    def display_game(self):
-        clear_screen()
-        print("Stock: {} cards".format(len(self.stock)))
-        print("Waste: {}".format(self.waste[-1] if self.waste else "Empty"))
-
-        print("\nFoundations:")
-        for i, foundation in enumerate(self.foundations):
-            print("Foundation {}: {}".format(i + 1, foundation[-1] if foundation else "Empty"))
-
-        print("\nTableau:")
-        max_pile_size = max(len(pile) for pile in self.tableau)
-        for row in range(max_pile_size):
-            pile_row = []
-            for pile in self.tableau:
-                if row < len(pile):
-                    card = pile[row]
-                    pile_row.append(str(card) if card.face_up else "X")
-                else:
-                    pile_row.append(" ")
-            print("  ".join(pile_row))
-
-    def play(self):
-        self.deal_initial_cards()
-
-        while True:
-            self.display_game()
-
-            move = input("Enter a move (e.g., 'stock', 'waste', 'tableau 1', 'foundation 1'): ")
-            move = move.lower().split()
-
-            if move[0] == "stock":
-                if self.stock:
-                    card = self.stock.pop()
-                    card.face_up = True
-                    self.waste.append(card)
-                else:
-                    print("Stock is empty.")
-            elif move[0] == "waste":
-                if self.waste:
-                    card = self.waste.pop()
-                    card.face_up = True
-                    self.stock.append(card)
-                else:
-                    print("Waste is empty.")
-            elif move[0] == "tableau":
-                pile_idx = int(move[1]) - 1
-                if pile_idx >= 0 and pile_idx < 7:
-                    if self.tableau[pile_idx]:
-                        card = self.tableau[pile_idx][-1]
-                        if card.face_up:
-                            self.waste.append(self.tableau[pile_idx].pop())
-                        else:
-                            card.face_up = True
-                    else:
-                        print("Tableau pile is empty.")
-                else:
-                    print("Invalid tableau pile.")
-            elif move[0] == "foundation":
-                foundation_idx = int(move[1]) - 1
-                if foundation_idx >= 0 and foundation_idx < 4:
-                    if self.waste:
-                        card = self.waste[-1]
-                        if card.rank == "A":
-                            if not self.foundations[foundation_idx]:
-                                self.foundations[foundation_idx].append(self.waste.pop())
-                            else:
-                                print("Invalid move. Foundation already has a card.")
-                        else:
-                            if self.foundations[foundation_idx]:
-                                last_card = self.foundations[foundation_idx][-1]
-                                if card.suit == last_card.suit and card.rank == str(int(last_card.rank) + 1):
-                                    self.foundations[foundation_idx].append(self.waste.pop())
-                                else:
-                                    print("Invalid move. Card doesn't follow the foundation's sequence.")
-                            else:
-                                print("Invalid move. Foundation requires an 'A' card to start.")
-                    else:
-                        print("Waste is empty.")
-                else:
-                    print("Invalid foundation pile.")
-            elif move[0] == "quit":
-                break
+    def print_aces(self):
+        print("Spades  Hearts  Dmnds  Clubs")
+        rtn_str = ""
+        for i in range(4):
+            if len(self.aces[i])==1:
+                rtn_str += self.aces[0][i].short() + "      "
             else:
-                print("Invalid move.")
+                rtn_str += "Empty   "
+        print(rtn_str)
 
-            print()
+    def gather_print_board_object(self):
+        print_board = []
+        for i in range(7):
+            print_board.append(["   "]*self.find_max_list())
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                char = "" if self.board[i][j].short()[0]=="1" else " "
+                print_board[i][j] = self.board[i][j].short() + char
+        return print_board
 
-            if self.is_game_won():
-                print("Congratulations! You have won the game!")
-                break
+    def find_max_list(self):
+        list_len = [len(i) for i in self.board]
+        return max(list_len)
 
-    def is_game_won(self):
-        return all(len(foundation) == 13 for foundation in self.foundations)
+def main():
+    """Set up game steps"""
+    game = Solitaire()
+    game.fresh_deal()
+    #while not game.win:
+    game.print_board()
+    game.print_aces()
+    ##get and validate user input
+    ##validate and users move
+    ##check win
+    print("main")
 
-
-# Start the game
-game = SolitaireGame()
-game.play()
-
+if __name__=="__main__":
+    main()
